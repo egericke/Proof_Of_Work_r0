ff0a61dd10a53649f85f32542dc6ac209aa3a818
egericke <70616620+egericke@users.noreply.github.com>
2025-02-25 14:49:22 -0500

    Update _app.js

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,13 +1,6 @@
-// web/pages/_app.js
-import { useEffect } from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  // Log to confirm the application is loading
-  useEffect(() => {
-    console.log('Dashboard application loaded successfully');
-  }, []);
-
   return <Component {...pageProps} />;
 }
 
