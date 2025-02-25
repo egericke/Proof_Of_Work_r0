ea3afa44db51c2e39628c2cfb6e7cf4d9838de18
egericke <70616620+egericke@users.noreply.github.com>
2025-02-25 14:48:33 -0500

    Update tailwind.config.js

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,15 +1,11 @@
-// web/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
