994fb8551f82bfe647e6686343b1f13e800795d8
egericke <70616620+egericke@users.noreply.github.com>
2025-02-28 23:18:39 -0500

    Update _app.js

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,7 +1,22 @@
+// web/pages/_app.js
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>My Daily Proof | Personal Dashboard</title>
+        <meta name="description" content="Personal fitness, productivity, and habit tracking dashboard" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Orbitron:wght@500;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
