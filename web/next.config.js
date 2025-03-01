117aa414d4763bf201b38956fd1bba19899f7abd
egericke <70616620+egericke@users.noreply.github.com>
2025-03-01 14:26:27 -0500

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: { unoptimized: true },
-  // Remove trailing slash setting if you don't need it
-  // trailingSlash: true,
+  // Remove the static export settings:
+  // output: 'export',
+  // images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
